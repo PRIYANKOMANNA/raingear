f4e2c3c81bd54b32c048a2842e958fe4458c7b2f
Priyanko Manna <priyanko.rishi@gmail.com>
2024-06-24 23:20:00 +0530

    2nd chat gpt course
    2nd chat gpt website added

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
 function addToCart(product) {
     alert(`${product} has been added to your cart!`);
 }
@@ -5,3 +27,7 @@ function addToCart(product) {
 function moreInfo(product) {
     alert(`More information about ${product}`);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
